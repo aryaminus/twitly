ee2e2206f0f2aec757791f9995870b55c65fa7ab
Sunim <sunim.54@gmail.com>
2017-06-16 20:02:13 +0545

    Importing validators

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.conf import settings
+from django.core.exceptions import ValidationError
+from .validators import validate_content
 # Create your models here.
 class Tweet(models.Model):
     user        = models.ForeignKey(settings.AUTH_USER_MODEL)
-    content     = models.CharField(max_length=140)
+    content     = models.CharField(max_length=140, validators=[validate_content])
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
 
