46347b9a93a6e6c4eaef9c88c4132acaa2943732
Sunim <sunim.54@gmail.com>
2017-06-17 19:43:10 +0545

    Build a create view html, create class meta in admin,import views in urls & manage views

--- tweets/admin.py
+++ tweets/admin.py
@@ -5,8 +5,8 @@ from .models import Tweet
 
 
 class TweetModelAdmin(admin.ModelAdmin):
-    form = TweetModelForm
-    # class Meta:
-    #     model = Tweet     
+    #form = TweetModelForm
+    class Meta:
+        model = Tweet
 
 admin.site.register(Tweet, TweetModelAdmin) 
--- tweets/views.py
+++ tweets/views.py
@@ -1,30 +1,28 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView
 
 from .models import Tweet
-# Create your views here.
-class TweetDetailView(DetailView):
-    #template_name = "tweets/detail_view.html"
-    queryset = Tweet.objects.all()
-
-#    def get_object(self):
-#       return Tweet.objects.get(id=1)
+from .forms import TweetModelForm
 
 
-class TweetListView(ListView):
-    queryset = Tweet.objects.all()
+class TweetCreateView(CreateView):
+    form_class = TweetModelForm
+    template_name = 'tweets/create_view.html'
+    success_url = "/tweet/create/"
 
-    def get_context_data(self, *args, **kwargs):
-        context = super(TweetListView, self).get_context_data(*args, **kwargs)
-        #context["another_list"] = Tweet.objects.all()
-        #print(context)
-        return context
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TweetCreateView, self).form_valid(form)
 
-def tweet_detail_view(request, pk=None): # pk == id
-    #obj = Tweet.objects.get(pk=pk) # GET from database
-    obj = get_object_or_404(Tweet, pk=pk)
-    print(obj)
+
+def tweet_create_view(request):
+    form = TweetModelForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.user = request.user
+        instance.save()
     context = {
-        "object": obj
+        "form":form
     }
-    return render(request, "tweets/detail_view.html", context)
+    return render(request, 'tweets/create_view.html', context)
+
