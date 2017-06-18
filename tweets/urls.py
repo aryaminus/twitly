4d416781754380fd8ebf17bc561eb9c26ecbc862
Sunim <sunim.54@gmail.com>
2017-06-18 19:14:58 +0545

    Add OwnerMixin in mixins,add updateview in urls, fix definations in views & manage htmls & migrate

--- tweets/urls.py
+++ tweets/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls import url
 
-#from .views import TweetListView, TweetDetailView, tweet_detail_view
 
 from .views import (
     TweetCreateView,
     TweetDetailView,
-    TweetListView
+    TweetListView,
+    TweetUpdateView
     )
 
 urlpatterns = [
     url(r'^$', TweetListView.as_view(), name='list'), # /tweet/
     url(r'^create/$', TweetCreateView.as_view(), name='create'), # /tweet/create/
-    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), 
+    url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # /tweet/1/
+    url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/1/
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,33 +1,47 @@
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView, ListView, CreateView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
 
-from .models import Tweet
 from .forms import TweetModelForm
-from .mixins import FormUserNeededMixin
+from .mixins import FormUserNeededMixin, UserOwnerMixin
+from .models import Tweet
+
+# Create
 
 class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = "/tweet/create/"
-
-"""
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super(TweetCreateView, self).form_valid(form)
+    #login_url = '/admin/'
 
 
-def tweet_create_view(request):
-    form = TweetModelForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.user = request.user
-        instance.save()
-    context = {
-        "form":form
-    }
-    return render(request, 'tweets/create_view.html', context)
-"""
+class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
+    queryset = Tweet.objects.all()
+    form_class = TweetModelForm
+    template_name = 'tweets/update_view.html'
+    success_url = "/tweet/"
 
 class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
+
+
+class TweetListView(ListView):
+    #template_name = "tweets/list_view.html"
+    queryset = Tweet.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        #context["another_list"] = Tweet.objects.all()
+        #print(context)
+        return context
+
+
+def tweet_detail_view(request, pk=None): # pk == id
+    #obj = Tweet.objects.get(pk=pk) # GET from database
+    obj = get_object_or_404(Tweet, pk=pk)
+    print(obj)
+    context = {
+        "object": obj
+    }
+    return render(request, "tweets/detail_view.html", context)
