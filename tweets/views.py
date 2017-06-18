ddf6a7e35f4afbb3796127b0de7077ab966901da
Sunim <sunim.54@gmail.com>
2017-06-18 19:06:45 +0545

    Add mixins for login & import in views & fix typos

--- tweets/views.py
+++ tweets/views.py
@@ -1,15 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView, ListView, CreateView
 
 from .models import Tweet
 from .forms import TweetModelForm
+from .mixins import FormUserNeededMixin
 
-
-class TweetCreateView(CreateView):
+class TweetCreateView(FormUserNeededMixin, CreateView):
     form_class = TweetModelForm
     template_name = 'tweets/create_view.html'
     success_url = "/tweet/create/"
 
+"""
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TweetCreateView, self).form_valid(form)
@@ -25,4 +27,7 @@ def tweet_create_view(request):
         "form":form
     }
     return render(request, 'tweets/create_view.html', context)
+"""
 
+class TweetDetailView(DetailView):
+    queryset = Tweet.objects.all()
