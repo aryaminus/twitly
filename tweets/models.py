4d576a65a0f21aaf81f6772785dc7cf02bacfbb5
Sunim <sunim.54@gmail.com>
2017-06-19 18:00:25 +0545

    ad reverse for url in models,remove success url from views,fet new mixin in mixins & change pattern in urls

--- tweets/models.py
+++ tweets/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from .validators import validate_content
+from django.urls import reverse
 # Create your models here.
 class Tweet(models.Model):
     user        = models.ForeignKey(settings.AUTH_USER_MODEL)
@@ -11,3 +12,6 @@ class Tweet(models.Model):
 
     def __str__(self):
         return str(self.content)
+ 
+    def get_absolute_url(self):
+        return reverse("tweet:detail", kwargs={"pk":self.pk})
